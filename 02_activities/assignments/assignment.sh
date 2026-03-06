209b6adc140c1eef04beb45aa4cfe7da1cb5e4fc
Silvia Margarian <silvia.margarian@mail.utoronto.ca>
2026-03-06 15:26:11 -0500

    The first 3 questions actually done

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -30,12 +30,12 @@ unzip -q rawdata.zip
 # 1. Create a directory named data
 mkdir data
 # 2. Move the ./rawdata directory to ./data/raw (eg. move it into ./data and rename it to raw)
-
+mv rawdata data
 # 3. List the contents of the ./data/raw directory
-
+ls ./data/rawdata
 # 4. Create the directory ./data/processed, 
 #    then create the following sub-directories within it: server_logs, user_logs, and event_logs
-
+ 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
